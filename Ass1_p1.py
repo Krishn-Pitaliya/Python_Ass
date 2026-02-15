4cbe100086777fd13a355984a7f6a41134a98abe
Krishn-Pitaliya <krishnapitaliya@gmail.com>
2026-02-15 10:23:23 +0530

    Delete Ass1_p1.py

--- Ass1_p1.py
+++
@@ -1,7 +0,0 @@
-no1=int(input("Enter the first number: "))
-no2=int(input("Enter the second number: "))
-
-print("Addition is:",no1+no2)
-print("Subtraction is:",no1-no2)
-print("Multiplication is:",no1*no2)
-print("Division is:",no1/no2)
